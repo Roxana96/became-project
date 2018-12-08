d51ead8ef7d664ba785ef1e790b27714d1ca3001
Roxana96 <botosaneanuroxana@yahoo.com>
2018-12-08 14:26:35 +0200

    Added more changes

---
+++ app.js
@@ -0,0 +1,32 @@
+/*function pow(x, n) {
+    if(n < 0) {
+        alert("Negative 'n' not supported");
+    } else {
+        let result = 1;
+
+        for (let i=0; i < n; i++) {
+            result *= x;
+        }
+
+        return result;
+    }
+}*/
+
+
+document.body.onkeyup = function (e){
+	if (e.keyCode === 13) item();
+}
+
+function item() {
+	const item = document.getElementById("input").value;
+	const ul = document.getElementById("list");
+	const li = document.createElement("li");
+	li.appendChild(document.createTextNode("-" + item));
+	ul.appendChild(li);
+	document.getElementById("input").value = "";
+	li.onclick = removeItem;
+}
+
+function removeItem(e) {
+	e.target.parentElement.removeChild(e.target);
+}
---
+++ test.js
@@ -0,0 +1,14 @@
+/*mocha.setup('bdd');
+const assert = chai.assert;
+
+describe('Pow', function() {
+    it('should work', function() {
+        assert.equal(pow(2, 3), 8);
+    })
+})
+
+mocha.run();*/
+
+describe("item function", () => {
+	it("should work like a charm!");
+})
